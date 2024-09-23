04e36a66bb4748724b8394a32f662d761a47bf9c
illurito <danielalberto3004@gmail.com>
2024-09-22 19:28:01 -0600

    actualizacion de seguridad

--- src/app/services/cloudinary.service.ts
+++ src/app/services/cloudinary.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import * as CryptoJS from 'crypto-js';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
